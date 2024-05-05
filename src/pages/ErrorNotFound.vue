6732c45cfb45938f419d7767a8ce68c07dcac4b2
harutyuna <harutyuna@energizeglobal.com>
2024-05-05 13:29:58 +0400

    development - Created error page

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,9 +1,18 @@
 <template>
-  <v-card>Page Not found</v-card>
-  <v-btn
-    to="/"
-    text="Back"
-  ></v-btn>
+  <v-sheet width="80%" class="text-center mx-auto">
+    <v-img
+      class="mx-auto"
+      :width="300"
+      aspect-ratio="16/9"
+      cover
+      src="../assets/404.svg"
+    ></v-img>
+    <v-btn
+      class="mx-auto"
+      to="/"
+      text="Home page"
+    ></v-btn>
+  </v-sheet>
 </template>
 <script setup>
 </script>
